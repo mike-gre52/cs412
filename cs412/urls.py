efabea944088a15a083b1ea850500e1d41cfdd78
mike-gre52 <greenemike52@gmail.com>
2025-02-13 21:20:10 -0500

    remove restaurant

--- cs412/urls.py
+++ cs412/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('hw/', include('hw.urls')),
     path('quotes/', include('quotes.urls')),
     path('formdata/', include('formdata.urls')),
-    path('restaurant/', include('restaurant.urls')),
+    #path('restaurant/', include('restaurant.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
